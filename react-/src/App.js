01f8e93dea12c8b74afdecc0303044941ab7aed7
aliyr <aliyr7592@gmail.com>
2019-01-27 11:47:10 +0330

    proxy server in react implemented

--- react-/src/App.js
+++ react-/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import VirtualScrollerExample from "./components/virtualScrollerExample";
+import ApiRequest from './components/apiRequest';
 
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height: '100%'}}>
         <VirtualScrollerExample/>
+        <ApiRequest/>
       </div>
     );
   }
