33a66c1700e3ad188aa3b6a2e26b0cd2afce5714
aliyr <aliyr7592@gmail.com>
2019-01-30 11:29:02 +0330

    some changes on build

--- react-/src/App.js
+++ react-/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import VirtualScrollerExample from "./components/virtualScrollerExample";
 import ApiRequest from './components/apiRequest';
