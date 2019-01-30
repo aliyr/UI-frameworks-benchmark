74cfe050098be70a55bdb204c858d42d3ffd5114
khashayar-pakkhesal <kh.p1999@yahoo.com>
2019-01-30 17:52:03 +0330

    with treeview

--- angular/src/app/tree-view/tree-view.component.ts
+++ angular/src/app/tree-view/tree-view.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-tree-view',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tree-view.component.scss']
 })
 export class TreeViewComponent implements OnInit {
+  title = 'angular-app-treeview';
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { id: 2, name: 'child1' },
+        { id: 3, name: 'child2' }
+      ]
+    },
+    {
+      id: 4,
+      name: 'root2',
+      children: [
+        { id: 5, name: 'child2.1' },
+        {
+          id: 6,
+          name: 'child2.2',
+          children: [
+            { id: 7, name: 'subsub' }
+          ]
+        }
+      ]
+    }
+  ];
+  options = {};
 
-  constructor() { }
+  constructor() {
 
-  ngOnInit() {
   }
+
+  ngOnInit() {}
 
 }
