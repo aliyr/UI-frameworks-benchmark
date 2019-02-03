115205cf67081b29ea45f9ca5adb6e0902a06176
Alious <aliyr7592@gmail.com>
2019-02-03 12:34:43 +0330

    state management platform enhanced

--- react-/src/components/stateManagement/Main.js
+++ react-/src/components/stateManagement/Main.js
@@ -1,10 +1,21 @@
 import React , {Component} from "react";
 
 class Main extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
     render() {
+        const todos = this.props.items.map((item, index) => {
+            return (
+                <div>{item}</div>
+            )
+        });
+
         return (
             <div>
-                Main Works!
+                {todos}
             </div>
         )
     }
--- react-/src/components/stateManagement/stateManagement.js
+++ react-/src/components/stateManagement/stateManagement.js
@@ -1,13 +1,16 @@
 import React , {Component} from "react";
 import Main from "./Main";
 import Navbar from "./Navbar";
+import {observer} from 'mobx-react';
+import TodoStore from '../../store/UsersStore';
 
-class StateManagement extends Component{
+@observer class StateManagement extends Component{
+
     render() {
         return (
             <div>
-                <Main/>
-                <Navbar/>
+                <Main items={TodoStore}/>
+                <Navbar />
             </div>
         )
     }
