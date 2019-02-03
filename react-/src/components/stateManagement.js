8e64a673961e8ee8c85ad303156e572c646e4cce
Alious <aliyr7592@gmail.com>
2019-02-03 11:15:06 +0330

    state management platform created

---
+++ react-/src/components/stateManagement.js
@@ -0,0 +1,13 @@
+import React , {Component} from "react";
+
+class StateManagement extends Component{
+    render() {
+        return (
+            <div>
+                state management works
+            </div>
+        )
+    }
+}
+
+export default StateManagement;
--- react-/src/router.jsx
+++ react-/src/router.jsx
@@ -3,35 +3,39 @@ import VirtualScrollerExample from "./components/virtualScrollerExample";
 import ApiRequest from "./components/apiRequest";
 import CheckboxWrapper from "./components/checkboxWrapper/checkboxWrapper";
 import React , {Component} from "react";
+import StateManagement from "./components/stateManagement";
 
  class RouterLinks extends Component{
-
-render() {
-    return (
-        <Router>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">home</Link>
-                    </li>
-                    <li>
-                        <Link to="/virtual/">virtual scroller</Link>
-                    </li>
-                    <li>
-                        <Link to="/api">api request</Link>
-                    </li>
-                    <li><Link to="/checkbox">checkbox</Link>
-                    </li>
-                </ul>
-                <Route path="/"  />
-                <Route path="/virtual" exact component={VirtualScrollerExample} />
-                <Route path="/api/" component={ApiRequest} />
-                <Route path="/checkbox/" component={CheckboxWrapper} />
-            </div>
-        </Router>
-    )
-}
-
+    render() {
+        return (
+            <Router>
+                <div>
+                    <ul>
+                        <li>
+                            <Link to="/">home</Link>
+                        </li>
+                        <li>
+                            <Link to="/virtual/">virtual scroller</Link>
+                        </li>
+                        <li>
+                            <Link to="/api">api request</Link>
+                        </li>
+                        <li>
+                            <Link to="/checkbox">checkbox</Link>
+                        </li>
+                        <li>
+                            <Link to="/state-management">state management</Link>
+                        </li>
+                    </ul>
+                    <Route path="/"  />
+                    <Route path="/virtual" exact component={VirtualScrollerExample} />
+                    <Route path="/api/" component={ApiRequest} />
+                    <Route path="/checkbox/" component={CheckboxWrapper} />
+                    <Route path="/state-management" component={StateManagement} />
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default RouterLinks;
