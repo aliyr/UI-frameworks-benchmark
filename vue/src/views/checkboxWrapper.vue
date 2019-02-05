7a102a781d4b5051e42600219fcefa8a8877f974
khashayar <kh.p1999@yahoo.com>
2019-02-05 18:33:08 +0330

    treeView

--- vue/src/views/checkboxWrapper.vue
+++ vue/src/views/checkboxWrapper.vue
@@ -1,42 +1,33 @@
 <template>
   <div>
-    
-  <div>
-    <div>{{value}}</div>
-      <Checkbox v-model="value">
+<div>    <button v-on:click="addMoreCheckBox">add 500 checkbox</button>
+</div>   
+    <Checkbox v-for="(element , index) in elements "  v-bind:id="index">
+        
         <img src="../assets/logo.png" alt="" class="img-logo">
       </Checkbox>
-  </div>
-
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aut quos tempora illo earum laborum rerum, dolore alias maiores cum fuga quod asperiores necessitatibus ea odio ducimus. Repellendus, perspiciatis quaerat?</p>
-       <div>
-         <div>{{value1}}</div>
-        <Checkbox v-model="value1">
-          <img src="../assets/logo.png" alt="" class="img-logo">
-        </Checkbox>
-       </div>
-<div>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet aut quos tempora illo earum laborum rerum, dolore alias maiores cum fuga quod asperiores necessitatibus ea odio ducimus. Repellendus, perspiciatis quaerat?</p>
-       <Checkbox v-model="value2">
-        <img src="../assets/logo.png" alt="" class="img-logo">
-      </Checkbox>
-</div>
      
   </div>
 </template>
 
 <script>
 import Checkbox from '../components/checkbox'
+import { method } from 'bluebird';
+import { METHODS } from 'http';
 export default {
   name: 'checkboxWrapper',
   components: { Checkbox },
   data: function () {
     return {
       value: false,
-            value1: false,
-                        value2: true
-
-
+      value1: false,
+      value2: true,
+      elements : new Array(0)
+    }
+  },
+  methods:{
+    addMoreCheckBox(){
+            this.elements = new Array(500)
     }
   }
 }
