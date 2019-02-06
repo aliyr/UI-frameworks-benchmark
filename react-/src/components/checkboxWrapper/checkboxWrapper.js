770569d0edcfc6ba38fd787da7a7d285d4fc36e7
khashayar <kh.p1999@yahoo.com>
2019-02-06 16:53:59 +0330

    changed to one checkbox and id  made by uuid

--- react-/src/components/checkboxWrapper/checkboxWrapper.js
+++ react-/src/components/checkboxWrapper/checkboxWrapper.js
@@ -2,61 +2,49 @@ import React, { Component } from 'react';
 import Checkbox from "./checkbox";
 import logo from '../../logo.svg';
 import Textholder from '../textholder'
+import uuid from "uuid";
 class CheckboxWrapper extends Component {
     constructor(props) {
         super(props);
         this.valueChangeFirst = this.valueChangeFirst.bind(this);
-        this.valueChangeSecond = this.valueChangeSecond.bind(this)
-        this.valueChangeThird = this.valueChangeThird.bind(this)
         this.addMoreCheckbox = this.addMoreCheckbox.bind(this)
+
     }
 
     state = {
+
         valueFirst: true,
         valueSecond:false,
         valueThird:true ,
-         data : new Array(0)
+         data : new Array(0),
+     
     };
 
     valueChangeFirst (e) {
         this.setState({valueFirst: e});
-        alert("value")
     }
-    valueChangeSecond (e) {
-        this.setState({valueSecond: e});
-        alert("value")
-    }
-    valueChangeThird (e) {
-        this.setState({valueThird: e});
-        alert("value")
-    }
+
     addMoreCheckbox(){
 this.setState(
-    this.state.data = new Array(500)
-      
+    this.state.data = new Array(500)   
 )
     }
     render() {
       
-
         for (let i = 0; i < this.state.data.length; i++) {
-             this.state.data[i] =   <Checkbox> {i}</Checkbox> ;
-            
+             this.state.data[i] =   <Checkbox> {i}</Checkbox> ; 
         }
 
         return (
             <div className="App">
-           <button onClick={this.addMoreCheckbox}>add 500 checkbox</button>
-                {/* <div>{this.state.value.toString()}</div> */}
-                {/* <Checkbox  id="first"> */}
-                    {/*<img style={{width: '100px'}} src={logo} className="App-logo" alt="logo" />*/}
-             
-                    {/* <Textholder fname="gholi"/> */}
-                {/* </Checkbox> */}
-                
+            <button onClick={this.addMoreCheckbox}>add 500 checkbox</button>
+
+                 <Checkbox id={uuid.v4()} checkboxValue={this.state.valueFirst} valueChanged={this.valueChangeFirst}>             
+                     <Textholder fname="gholi"/> 
+                 </Checkbox> 
                 
                 {this.state.data}
-            </div>
+            </div> 
         );
     }
 }
