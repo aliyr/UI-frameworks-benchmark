ab546579fc968580605970f6a1f932aaa572e747
khashayar <kh.p1999@yahoo.com>
2019-02-07 12:04:01 +0330

    new persist store for react

--- react-/src/components/Store.js
+++ react-/src/components/Store.js
@@ -1,34 +1,88 @@
-import { observable } from 'mobx';
+import { observable ,action } from 'mobx';
+import { persist ,create} from 'mobx-persist';
+// class UsersStore {
+
+//      @observable users = [];
+//     @observable company = "";
+//     @observable draftedItem = "gholi";
+//     getUsers() {
+//         this.users = [
+//             { id: 1, name: 'aaa' },
+//             { id: 2, name: 'bbb' }
+//         ];
+
+//         return this.users;
+//     }
+
+//     updateUser(i) {
+//         this.users[i].name = this.draftedItem;
+
+//     }
+
+//     getCompany() {
+//         this.company = "company";
+//         return this.company;
+//     }
+
+//     addUser(name) {
+//         let rnd = Math.random(100);
+//         this.users.push({ id: rnd, name: name })
+//     }
+// }
+// class storePersist {
+
+// }
+// const persistStore = create({
+//     storage: localStorage 
+// })
+// const usersStore = new UsersStore();
+
+// export default usersStore;
+// export  const someStore = persistStore('some', new storePersist);
+
+
 
 class UsersStore {
-    @observable users = [];
-    @observable company = "";
-    @observable draftedItem = "gholi";
-    getUsers() {
-        this.users = [
-            { id: 1, name: 'aaa' },
-            { id: 2, name: 'bbb' }
-        ];
 
-        return this.users;
+    @persist('list')  @observable users = [];
+       @persist  @observable company = "";
+       @persist  @observable draftedItem = "gholi";
+        getUsers() {
+            this.users = [
+                { id: 1, name: 'aaa' },
+                { id: 2, name: 'bbb' }
+            ];
+    
+            return this.users;
+        }
+        updateUser(i) {
+            this.users[i].name = this.draftedItem;
+    
+        }
+        getCompany() {
+            this.company = "company";
+            return this.company;
+        }
+
+        addUser(name) {
+            let rnd = Math.random(100);
+            this.users.push({ id: rnd, name: name })
+        }
     }
+   
+    const hydrate = create({
+        storage: localStorage,
+    })
 
-    updateUser(i) {
-        this.users[i].name = this.draftedItem;
+    const someStore = new UsersStore()
+    export default someStore;
+    hydrate('some', someStore)
+    .then(() => console.log('some hydrated'))
 
-    }
+ const result = hydrate('some', someStore)
+const rehydrate = result.rehydrate
+result.then(() => console.log('some hydrated'))
 
-    getCompany() {
-        this.company = "company";
-        return this.company;
-    }
-
-    addUser(name) {
-        let rnd = Math.random(100);
-        this.users.push({ id: rnd, name: name })
-    }
-}
-
-const usersStore = new UsersStore();
-
-export default usersStore;
+setTimeout(() => {
+    rehydrate().then(() => console.log('rehydrated'))
+}, 3000)
