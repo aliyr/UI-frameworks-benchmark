957308a63c8c97702274778227026009f813a877
khashayar <kh.p1999@yahoo.com>
2019-02-07 13:26:37 +0330

    beautified code in store

--- react-/src/components/Store.js
+++ react-/src/components/Store.js
@@ -6,46 +6,6 @@ import {
     persist,
     create
 } from 'mobx-persist';
-// class UsersStore {
-
-//      @observable users = [];
-//     @observable company = "";
-//     @observable draftedItem = "gholi";
-//     getUsers() {
-//         this.users = [
-//             { id: 1, name: 'aaa' },
-//             { id: 2, name: 'bbb' }
-//         ];
-
-//         return this.users;
-//     }
-
-//     updateUser(i) {
-//         this.users[i].name = this.draftedItem;
-
-//     }
-
-//     getCompany() {
-//         this.company = "company";
-//         return this.company;
-//     }
-
-//     addUser(name) {
-//         let rnd = Math.random(100);
-//         this.users.push({ id: rnd, name: name })
-//     }
-// }
-// class storePersist {
-
-// }
-// const persistStore = create({
-//     storage: localStorage 
-// })
-// const usersStore = new UsersStore();
-
-// export default usersStore;
-// export  const someStore = persistStore('some', new storePersist);
-
 
 
 class UsersStore {
