189fd8812646fe5a5baf3ec6509d0b6377d6497d
Alious <aliyr7592@gmail.com>
2019-02-12 10:49:45 +0330

    Code modifying in react login page

--- react-/src/components/stateManagement/loginPage.js
+++ react-/src/components/stateManagement/loginPage.js
@@ -1,7 +1,6 @@
 import React , {Component} from "react";
 import {observer} from "mobx-react";
 import someStore from '../Store';
-import {BrowserRouter as Router} from "react-router-dom";
 
 @observer class loginForm extends Component{
     constructor(props) {
@@ -10,21 +9,20 @@ import {BrowserRouter as Router} from "react-router-dom";
     }
     state = {
         name : '',
-        usernullCheck: false,
         loginCHeck : '',
         userIsValid :null
     };
     verifyUser(loginName){
-   
+
          this.state.loginCHeck = someStore.users.find((username) =>  {
-           if(username.name == loginName){
-            this.setState({userIsValid : true})
+           if(username.name === loginName){
+            this.setState({userIsValid : true});
             return ( username)
             }else{
             this.setState({userIsValid : false})
                   }
-            })
-          
+            });
+
 this.forceUpdate()
 
     }
@@ -32,23 +30,23 @@ this.forceUpdate()
         this.setState({ name: e.target.value });
     }
     render(){
-        let result
+        let result;
         if(this.state.userIsValid){
             this.props.history.push({
                 pathname : `/profile/${this.state.loginCHeck.id}`
-            })
+            });
 
            result =  <div>    {this.state.loginCHeck.id}</div>
-        }else if(this.state.loginCHeck == undefined){
+        }else if (this.state.loginCHeck === undefined) {
             result =  <div>  your username is not valid</div>
         }
         return(
             <div>
-                <input type='text' value={this.state.name}  onChange={ this.changeNameValue.bind(this) } ></input>
-                    <button onClick={() => this.verifyUser(this.state.name)} >log in </button>
-           <div>
-           {result}
-           </div>
+               <input type='text' value={this.state.name}  onChange={ this.changeNameValue.bind(this) } />
+               <button onClick={() => this.verifyUser(this.state.name)} >log in </button>
+               <div>
+                {result}
+               </div>
             </div>
         )
     }
