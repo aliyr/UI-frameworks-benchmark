54de66d2d5f73241a246f2a294f63bb5783e5c02
Alious <aliyr7592@gmail.com>
2019-02-17 19:14:07 +0330

    Vue test completed

--- vue/tests/unit/loginForm.spec.js
+++ vue/tests/unit/loginForm.spec.js
@@ -1,12 +1,13 @@
-import { mount, createLocalVue } from '@vue/test-utils'
+import { mount, createLocalVue, shallowMount } from '@vue/test-utils'
 import LoginForm from '../../src/views/LoginForm'
 import VueRouter from 'vue-router'
 import routes from '../../src/router'
-import State from '../../src/store'
+import Store from '../../src/store'
 import Vuex from 'vuex'
 
 const localVue = createLocalVue()
-localVue.use(VueRouter, State)
+localVue.use(VueRouter)
+localVue.use(Store)
 
 describe('LoginForm.vue', () => {
   it('should render button', () => {
@@ -19,11 +20,20 @@ describe('LoginForm.vue', () => {
   })
   it('should gets the wrong name and shows the not found error', () => {
     const router = new VueRouter(routes)
-    const store = new Vuex.Store(State)
+    const store = new Vuex.Store(Store)
     const wrapper = mount(LoginForm, { localVue, router, store })
     const button = wrapper.find('button')
     wrapper.setData({ name: 'gholi' })
     button.trigger('click')
     expect(wrapper.contains('div#error')).toBe(true)
+  })
+  it('should gets the right name and redirects to profile page', () => {
+    const store = new Vuex.Store(Store)
+    const router = new VueRouter(routes)
+    const wrapper = shallowMount(LoginForm, { localVue, store, router, propsData: { userInput: 'gholi' } })
+    const button = wrapper.find('button')
+    wrapper.vm.addUser('gholi')
+    button.trigger('click')
+    expect(wrapper.vm.$data.errorMessage.length).toBe(0)
   })
 })
