c2580f460a2903d5b2848e0567f5ca2ff99e9238
khashayar <kh.p1999@yahoo.com>
2019-02-19 12:32:04 +0330

    angular codes beautified

--- angular/src/app/login-page/login-page.component.spec.ts
+++ angular/src/app/login-page/login-page.component.spec.ts
@@ -1,57 +1,62 @@
-import { async, TestBed } from '@angular/core/testing';
-import { LoginPageComponent } from './login-page.component';
-import { AppComponent } from '../app.component';
+import {async,TestBed} from '@angular/core/testing';
+import {LoginPageComponent} from './login-page.component';
+import {AppComponent} from '../app.component';
 import {RouterTestingModule} from "@angular/router/testing";
-import {ProfileComponent } from '../profile/profile.component'
+import {ProfileComponent} from '../profile/profile.component'
 
 fdescribe('ProfileComponent', () => {
 
-  beforeEach(async(() => {
+  beforeEach(async (() => {
 
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
         RouterTestingModule.withRoutes(
-          [{path: '', component: AppComponent}, 
-          {path: 'login-page', component: LoginPageComponent},
-          {path: 'profile/:id', component: ProfileComponent}
-        ]
+          [{
+              path: '',
+              component: AppComponent
+            },
+            {
+              path: 'login-page',
+              component: LoginPageComponent
+            },
+            {
+              path: 'profile/:id',
+              component: ProfileComponent
+            }
+          ]
         )
-
       ],
       declarations: [
         LoginPageComponent,
         AppComponent,
         ProfileComponent
-        
+
       ],
-       providers: [
-       ],
+      providers: [],
     }).compileComponents();
-   
+
   }));
 
-it('should check if component is created', ()=>{
-  const fixture = TestBed.createComponent(LoginPageComponent);
-  expect(fixture).toBeDefined();
-})
+  it('should check if component is created', () => {
+    const fixture = TestBed.createComponent(LoginPageComponent);
+    expect(fixture).toBeDefined();
+  })
 
 
   it(' check if button is created ', () => {
-      const fixture = TestBed.createComponent(LoginPageComponent);
-      const compiled = fixture.debugElement.nativeElement;
-      expect(compiled.querySelector('button')).toBeDefined()
- 
+    const fixture = TestBed.createComponent(LoginPageComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('button')).toBeDefined()
   });
 
-
-it('should check if after button is clicked user verification is working and route change' , async() => {
-       const fixture = TestBed.createComponent(LoginPageComponent );
-        const compiled = fixture.debugElement.nativeElement;
-        fixture.detectChanges()
-         compiled.querySelector('textarea').textContent ="aaa"
-          compiled.querySelector('button').click()
-         fixture.detectChanges()
-         expect(fixture.componentInstance.WilRouteChange).toBe(true)
+  it('should check if after button is clicked user verification is working and route change', async () => {
+    const fixture = TestBed.createComponent(LoginPageComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    fixture.detectChanges()
+    compiled.querySelector('textarea').textContent = "aaa"
+    compiled.querySelector('button').click()
+    fixture.detectChanges()
+    expect(fixture.componentInstance.WilRouteChange).toBe(true)
   })
 });
